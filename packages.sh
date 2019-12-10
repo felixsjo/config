bfeb71703c03831bce8ab274a1995c53bb28e37c
Felix <felix.sjqv@gmail.com>
2019-12-10 22:06:51 +0100

    add: packages installing script

---
+++ packages.sh
@@ -0,0 +1,22 @@
+echo "Installing packages..."
+
+pacman -S --needed alacritty arc-gtk-theme arc-icon-theme base chromium evince feh gcc gimp git gpick i3-gaps i3blocks i3lock i3status lightdm lightdm-gtk-greeter lxappearance make man-db man-pages neofetch neovim network-manager-applet networkmanager noto-fonts noto-fonts-extra pandoc picom pulseaudio python-pip ranger rofi scrot sudo texinfo texlive-core thunar ttf-font-awesome vlc which xdg-user-dirs xdotool xorg xorg-xinit
+
+if [ -e $(command -v yay) ]
+then
+  echo "yay already installed, do nothing"
+else
+  echo "installing yay"
+  git clone https://aur.archlinux.org/yay.git ~/
+  cd ~/yay
+  makepkg -si
+  cd ~/
+fi
+
+echo "Installing yay packages..."
+if [ -e $(command -v indicator-sound-switcher) ]
+then
+  echo "indicator-sound-switcher already installed, do nothing"
+else
+  yay indicator-sound-switcher
+fi
