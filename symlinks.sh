848c90502ab1a418cdf17f0433587f0b2f90d678
Felix <felix.sjqv@gmail.com>
2019-12-11 00:30:54 +0100

    fix: wrong symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,7 +1,7 @@
 #dependecies: i3, i3blocks, gtk2, gtk3, neovim
-ln -s gtk-2.0 ~/.config/gtk-2.0
-ln -s gtk-3.0 ~/.config/gtk-3.0
-ln -s i3 ~/.config/i3
-ln -s i3blocks ~/.config/i3blocks
-ln -s i3blocks-contrib ~/i3blocks-contrib
-ln -s nvim ~/.config/nvim
+ln -s -f ~/config/gtk-2.0 ~/.config/gtk-2.0
+ln -s -f ~/config/gtk-3.0 ~/.config/gtk-3.0
+ln -s -f ~/config/i3 ~/.config/i3
+ln -s -f ~/config/i3blocks ~/.config/i3blocks
+ln -s -f ~/config/i3blocks-contrib ~/i3blocks-contrib
+ln -s -f ~/config/nvim ~/.config/nvim
