7ba139d43397a9d88f4d08c932e58a7776fb857f
Felix <felix.sjqv@gmail.com>
2019-12-11 18:25:36 +0100

    add: packages

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 echo "Installing packages..."
 
-pacman -S --needed alacritty arc-gtk-theme arc-icon-theme base chromium evince feh gcc gimp git gpick i3-gaps i3blocks i3lock i3status lightdm lightdm-gtk-greeter lxappearance make man-db man-pages neofetch neovim network-manager-applet networkmanager noto-fonts noto-fonts-extra pandoc picom pulseaudio python-pip ranger rofi scrot sudo texinfo texlive-core thunar ttf-font-awesome vlc which xdg-user-dirs xdotool xorg xorg-xinit
+pacman -S --needed blueman libreoffice-still htop flameshot eog evolution deluge alacritty arc-gtk-theme arc-icon-theme base chromium evince feh gcc gimp git gpick i3-gaps i3blocks i3lock i3status lightdm lightdm-gtk-greeter lxappearance make man-db man-pages neofetch neovim network-manager-applet networkmanager noto-fonts noto-fonts-extra pandoc picom pulseaudio python-pip ranger rofi scrot sudo texinfo texlive-core thunar ttf-font-awesome vlc which xdg-user-dirs xdotool xorg xorg-xinit
 
 echo "Installing yay..."
 if [ -e $(command -v yay) ]
