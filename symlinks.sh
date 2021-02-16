61a17ff0b28ff14855bdaffba6bd89adc812f09c
Felix Sjoqvist <felix.sjqv@gmail.com>
2021-02-17 00:13:06 +0100

    new

--- symlinks.sh
+++ symlinks.sh
@@ -1,8 +1,8 @@
 #dependecies: i3, i3blocks, gtk2, gtk3, neovim
 #ln -s -f ~/config/gtk-2.0 ~/.config/gtk-2.0
 #ln -s -f ~/config/gtk-3.0 ~/.config/gtk-3.0
-#ln -s -f ~/config/i3 ~/.config/i3
-#ln -s -f ~/config/i3blocks ~/.config/i3blocks
+ln -s -f ~/config/i3/config ~/.config/i3/config
+ln -s -f ~/config/i3blocks/config ~/.config/i3blocks/config
 #ln -s -f ~/config/i3blocks-contrib ~/i3blocks-contrib
-ln -s -f ~/config/nvim ~/.config/nvim
+ln -s -f ~/config/nvim/init.vim ~/.config/nvim/init.vim
 ln -s -f ~/config/.tmux.conf ~/.tmux.conf
